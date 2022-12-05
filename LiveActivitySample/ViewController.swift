51fa6561de1114c6713f42affe2ca04f303557e5
fhboswell <franklin.henry.boswell@gmail.com>
2022-12-05 11:20:47 -0800

    Initial Commit

---
+++ LiveActivitySample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveActivitySample
+//
+//  Created by Henry Boswell on 12/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
